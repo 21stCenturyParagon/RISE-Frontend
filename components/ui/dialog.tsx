f2d59eab92e525814dc80b73ed979d812f5f8927
21stCenturyParagon <ahsannayaz@outlook.com>
2024-12-09 09:51:06 +0400

    fix error

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -11,12 +11,11 @@ const Dialog = DialogPrimitive.Root
 const DialogTrigger = DialogPrimitive.Trigger
 
 const DialogPortal = ({
-  className,
-  ...props
-}: DialogPrimitive.DialogPortalProps) => (
-  <DialogPrimitive.Portal className={cn(className)} {...props} />
-)
-DialogPortal.displayName = DialogPrimitive.Portal.displayName
+    ...props
+  }: DialogPrimitive.DialogPortalProps) => (
+    <DialogPrimitive.Portal {...props} />
+  )
+  DialogPortal.displayName = DialogPrimitive.Portal.displayName
 
 const DialogOverlay = React.forwardRef<
   React.ElementRef<typeof DialogPrimitive.Overlay>,
