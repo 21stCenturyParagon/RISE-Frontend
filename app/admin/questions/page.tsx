e6ec4b6a18f2552b34a6a49c1b21cbbb89f17985
21stCenturyParagon <ahsannayaz@outlook.com>
2024-12-19 19:50:06 +0400

    fix difficulty values

--- app/admin/questions/page.tsx
+++ app/admin/questions/page.tsx
@@ -401,9 +401,9 @@ export default function AllQuestionsPage() {
                   <SelectValue placeholder="Select difficulty" />
                 </SelectTrigger>
                 <SelectContent>
-                  <SelectItem value="easy">Easy</SelectItem>
-                  <SelectItem value="medium">Medium</SelectItem>
-                  <SelectItem value="hard">Hard</SelectItem>
+                  <SelectItem value="Easy">Easy</SelectItem>
+                  <SelectItem value="Medium">Medium</SelectItem>
+                  <SelectItem value="Hard">Hard</SelectItem>
                 </SelectContent>
               </Select>
               <Input
